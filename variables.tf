28f1f8a43e4121f4d753d6da83c507033fd7d05c
Julian Nonino <jnonino@users.noreply.github.com>
2022-05-19 22:00:00 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,12 @@ variable "aws_principals_identifiers" {
   type        = list(string)
   description = "List of identifiers for AWS principals with access to write in the logs bucket"
 }
+
+#------------------------------------------------------------------------------
+# S3
+#------------------------------------------------------------------------------
+variable "enable_s3_bucket_server_side_encryption" {
+  description = "(Optional) If true, server side encryption will be applied."
+  type        = bool
+  default     = false
+}
