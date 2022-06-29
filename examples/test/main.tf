0ab27b3bdac9a23a5cbd716f64a66cf3f8bb1485
Julian Nonino <noninojulian@gmail.com>
2022-06-29 12:01:05 +0100

    Set logs encryptions to SSE-S3

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -5,6 +5,5 @@ module "logs_bucket" {
   aws_principals_identifiers                     = ["test-user-arn"]
   block_s3_bucket_public_access                  = true
   enable_s3_bucket_server_side_encryption        = true
-  s3_bucket_server_side_encryption_sse_algorithm = "aws:kms"
-  s3_bucket_server_side_encryption_key           = "aws/s3"
+  s3_bucket_server_side_encryption_sse_algorithm = "AES256"
 }
